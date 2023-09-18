da2e4de20f8d72813eb91eef7347976d87d56c70
Your Name <you@example.com>
2023-09-18 12:01:28 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+
+
+$('.slick').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 1000,
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+
+        }
+    }
+    ]
+});
+
+$('.slick2').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    Infinity: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+});
+
